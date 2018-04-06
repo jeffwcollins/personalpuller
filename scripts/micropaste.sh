84d74a19901dfd8e00edfadf1913e9de5d102298
jeff <jeff@Media-Server.bonezie.com>
2018-04-06 12:37:40 -0500

    micropaste download file

---
+++ scripts/micropaste.sh
@@ -0,0 +1,30 @@
+
+#!/bin/sh
+DIR=$( cd "$(dirname "$0")" ; pwd -P )
+. $DIR/env.sh
+rm $LIST $LIST2 $LIST3 $LIST4 $LIST5
+LIST=`mktemp`
+#LIST2=`mktemp`
+#LIST3=`mktemp`
+LIST4=`mktemp`
+LIST5=`mktemp`
+
+for i in {0..100..1}; do http_proxy="" https_proxy="" wget --no-check-certificate -O - https://micropaste.com/lists/$i -O - | grep -o -P "micropaste.com/view/.{0,8}"; done | awk -F'/' '{print $3}' >> $LIST
+LIST2=$(while read p; do echo https://micropaste.com/view/raw/$p; done < $LIST)
+#echo $LIST2 > $BASEDIR/"MICROPASTESITES$(date +"%FT%H%M%").txt"
+#$SCRIPTDIR/push_list
+LIST3=$(while read p; do wget --no-check-certificate -O $p -O -; done < $LIST2)
+$SCRIPTDIR/purify.sh $LIST3 > $LIST4
+NUMBER=`wc -l $LIST4 | tr -s ' ' | cut -f 1 -d ' '`
+echo "Harvested $NUMBER onion links..."
+$SCRIPTDIR/push_list.sh $LIST4
+#
+
+rm $LIST $LIST2 $LIST3 $LIST4 $LIST5
+
+#if [ $FOUND -gt 0 ]; then
+#	(
+#	cd $BASEDIR
+#	scrapy crawl tor -a load_links=$PURIFIED -a test=yes
+#)
+#fi;
