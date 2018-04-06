70a6fa638bde1bde6b68c978a45bb79c2381940e
jeffwcollins <jeffwcollins@gmail.com>
2018-04-06 15:30:49 -0500

    finally works

--- scripts/micropaste.sh
+++ scripts/micropaste.sh
@@ -9,14 +9,16 @@ LIST3=`mktemp`
 LIST4=`mktemp`
 LIST5=`mktemp`
 
-for i in {0..100..1}; do http_proxy="" https_proxy="" wget --no-check-certificate -O - https://micropaste.com/lists/$i -O - | grep -o -P "micropaste.com/view/.{0,8}"; done | awk -F'/' '{print $3}' >> $LIST
-while read p; do echo "https://micropaste.com/view/raw/$p"; done < $LIST | >> $LIST2
-#echo $LIST2 > $BASEDIR/"MICROPASTESITES$(date +"%FT%H%M%").txt"
+for i in {0..1000..1}; do http_proxy="" https_proxy="" wget --no-check-certificate -O - https://micropaste.com/lists/$i -O - | grep -o -P "micropaste.com/view/.{0,8}"; done | awk -F'/' '{print $3}' >> $LIST
+cat $LIST | while read p; do echo https://micropaste.com/view/raw/$p >> $LIST2; done
+#cat $LIST2 > $BASEDIR/"MICROPASTESITES$(date +"%FT%H%M%").txt"
 #$SCRIPTDIR/push_list
-while read p; do http_proxy="" https_proxy="" wget --no-check-certificate -O - $p; done < $LIST2 | >> $LIST3
+cat $LIST2 | while read p; do http_proxy="" https_proxy="" wget --no-check-certificate -O - $p >> $LIST3; done
 $SCRIPTDIR/purify.sh $LIST3 > $LIST4
 NUMBER=`wc -l $LIST4 | tr -s ' ' | cut -f 1 -d ' '`
 echo "Harvested $NUMBER onion links..."
+#cat $LIST3 >> list3
+#cat $LIST4 >> list4
 $SCRIPTDIR/push_list.sh $LIST4
 #
 
