34801c70006e2b229763ca579322fbf3705ba130
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-07-27 11:00:03 -0400

    Create pull_list.sh

---
+++ scripts/pull_list.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+DIR=$( cd "$(dirname "$0")" ; pwd -P )
+. $DIR/env.sh
+LIST=`mktemp`
+LIST2=`mktemp`
+TOR2WEB_JSON=`mktemp`
+http_proxy="" https_proxy="" wget --no-check-certificate -O $TOR2WEB_JSON https://eqt5g4fuenphqinx.tor2web.org/antanistaticmap/stats/yesterday
+$SCRIPTDIR/import_tor2web.py $TOR2WEB_JSON > $LIST
+rm $TOR2WEB_JSON
+
+curl --proxy 127.0.0.1:3129 http://zlal32teyptf4tvi.onion/json/all >> $LIST
+$SCRIPTDIR/purify.sh $LIST > $LIST2
+NUMBER=`wc -l $LIST2 | tr -s ' ' | cut -f 1 -d ' '`
+echo "Harvested $NUMBER onion links..."
+$SCRIPTDIR/push_list.sh $LIST2
+rm $LIST $LIST2
