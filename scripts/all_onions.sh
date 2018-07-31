14885fe9925451251d5f1d8bc2e12966b6ddc1d0
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-07-31 11:16:28 -0500

    Update all_onions.sh

--- scripts/all_onions.sh
+++ scripts/all_onions.sh
@@ -2,14 +2,12 @@
 DIR=$( cd "$(dirname "$0")" ; pwd -P )
 . $DIR/env.sh
 rm $LIST $LIST2 $LIST3 $LIST4 $LIST5
+#
 LIST=`mktemp`
 LIST2=`mktemp`
 LIST3=`mktemp`
-LIST4=`mktemp`
-LIST5=`mktemp`
-
-for i in {0..10..1}; echo 'https://web.archive.org/web/20170711212802/https://onions.system33.pw/?page=$i' >> $List; done
-cat $LIST | while read p; do http_proxy="" https_proxy="" wget --no-check-certificate --tries=1 -O - $p -O - | grep -E -o '[0-9a-zA_Z]+\.onion' >> $LIST2; done
+#
+for i in {0..10..1}; echo 'https://web.archive.org/web/20170711212802/https://onions.system33.pw/?page=$i' >> $List; cat $LIST | while read p; do http_proxy="" https_proxy="" wget --no-check-certificate -O - $p >> $List2; done
 $SCRIPTDIR/purify.sh $LIST2 > $LIST3
 NUMBER=`wc -l $LIST3 | tr -s ' ' | cut -f 1 -d ' '`
 echo "Harvested $NUMBER onion links..."
