c9340f329a575af132a30336a5882ddad92bb871
root <root@tor-server2.bonezie.com>
2018-08-16 18:15:44 +0000

    update

--- scripts/all_onions.sh
+++ scripts/all_onions.sh
@@ -1,28 +1,11 @@
 #!/bin/sh
 DIR=$( cd "$(dirname "$0")" ; pwd -P )
 . $DIR/env.sh
-rm $LIST $LIST2 $LIST3 $LIST4 $LIST5
+#LIST4=`mktemp`
+#LIST5=`mktemp`
+#LIST6=`mktemp`
 #
-LIST=`mktemp`
-LIST2=`mktemp`
-LIST3=`mktemp`
-#
-for i in {0..10..1}; do
-  echo 'https://web.archive.org/web/20170711212802/https://onions.system33.pw/?page=$i' >> domainlist.txt;
+for i in $(seq 1 1000000); do
+  echo "http://jld3zkuo4b5mbios.onion/?page=$i" >> domainlist;
 done
-
-#cat $List | while read p; do
-#  http_proxy="" https_proxy="" wget --no-check-certificate -O - $p | grep -E -o '[0-9a-zA_Z]+\.onion' >> $List2;
-#done
-#
-#$SCRIPTDIR/purify.sh $LIST2 > $LIST3
-#NUMBER=`wc -l $LIST3 | tr -s ' ' | cut -f 1 -d ' '`
-#echo "Harvested $NUMBER onion links..."
-#(
-#cd $BASEDIR
-#scrapy crawl tor -a load_links=$LIST3 -a test=yes
-#)
-rm $LIST $LIST2 $LIST3
-#
-#9444732965739290427392
 #
