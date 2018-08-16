e316b4d333a05373c631e500010805da7d6e83fe
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-08-16 14:13:15 -0500

    Create all_onions_loop.sh

---
+++ scripts/all_onions_loop.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+DIR=$( cd "$(dirname "$0")" ; pwd -P )
+. $DIR/env.sh
+LIST=`mktemp`
+LIST2=`mktemp`
+#
+for i in $(seq 1 10); do
+$SCRIPTDIR/tor_extract_from_url.sh "http://jld3zkuo4b5mbios.onion/?page=$i" >> $LIST
+$SCRIPTDIR/purify.sh $LIST >> $LIST2
+NUMBER=`wc -l $LIST2 | tr -s ' ' | cut -f 1 -d ' '`
+echo "Harvested $NUMBER onion links..."
+wait 30s
+$SCRIPTDIR/push_list.sh $LIST2
+rm $LIST $LIST2
+done
+#
