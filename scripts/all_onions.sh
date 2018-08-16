8464b6f8361fada39ae86bde39dc0bbd2b0b4c8d
jeffwcollins <jeffwcollins@users.noreply.github.com>
2018-08-16 13:55:35 -0500

    Update all_onions.sh

--- scripts/all_onions.sh
+++ scripts/all_onions.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 DIR=$( cd "$(dirname "$0")" ; pwd -P )
 . $DIR/env.sh
-#LIST4=`mktemp`
-#LIST5=`mktemp`
+LIST=`mktemp`
+LIST2=`mktemp`
 #LIST6=`mktemp`
 #
-for i in $(seq 1 1000000); do
-  echo "http://jld3zkuo4b5mbios.onion/?page=$i" >> domainlist;
+for i in $(seq 1 1000); do
+  $SCRIPTDIR/tor_extract_from_url.sh "http://jld3zkuo4b5mbios.onion/?page=$i" >> $LIST
 done
+$SCRIPTDIR/purify.sh $LIST >> $LIST2
+NUMBER=`wc -l $LIST2 | tr -s ' ' | cut -f 1 -d ' '`
+echo "Harvested $NUMBER onion links..."
+sleep 1m
+$SCRIPTDIR/push_list.sh $LIST2
+rm $LIST $LIST2
 #
